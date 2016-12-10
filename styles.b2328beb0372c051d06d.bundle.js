6fca8d310817280d77422e4f03958d49639d2810
Jayant <you@example.com>
2016-12-10 02:09:50 -0800

    testing material

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,3],{255:function(n,o){},492:function(n,o,c){n.exports=c(255)}},[492]);
+webpackJsonp([1,3],{357:function(n,o){},660:function(n,o,c){n.exports=c(357)}},[660]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
