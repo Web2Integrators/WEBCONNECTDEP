3a40a25ad755a3a0f3b929e3333a8c6814a639bf
Jayant <you@example.com>
2016-12-10 08:13:09 -0800

    lots of chnages

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,3],{403:function(n,o){},721:function(n,o,c){n.exports=c(403)}},[721]);
+webpackJsonp([2,4],{1054:function(n,o,c){n.exports=c(486)},486:function(n,o){}},[1054]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
