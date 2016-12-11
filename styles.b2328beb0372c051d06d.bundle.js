8a3db055b9b072965d6ae3c872fbc93dbc887941
Jayant <you@example.com>
2016-12-11 08:27:08 -0800

    spiiner deploy

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([2,4],{1054:function(n,o,c){n.exports=c(486)},486:function(n,o){}},[1054]);
+webpackJsonp([2,4],{1056:function(n,o,c){n.exports=c(487)},487:function(n,o){}},[1056]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
